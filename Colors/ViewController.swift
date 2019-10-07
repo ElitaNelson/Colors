58e250b4649cea5d17376f7136bb6a5c53814819
Elita Nelson <ezn526@mail.missouri.edu>
2019-10-06 23:52:23 -0500

    First commit

---
+++ Colors/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Colors
+//
+//  Created by Elita Nelson on 10/6/19.
+//  Copyright © 2019 Elita Nelson. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
